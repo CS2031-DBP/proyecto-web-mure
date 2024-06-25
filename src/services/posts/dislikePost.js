f37afceb252d5a3391b5560d86c1be229577210b
joaquinsalinas06 <joaquin.salinas@utec.edu.pe>
2024-06-25 11:43:05 -0500

    Añadido Funcionalidad Likear

---
+++ src/services/posts/dislikePost.js
@@ -0,0 +1,16 @@
+import Api from "../api";
+
+const api = new Api({})
+
+export async function dislikePost(postId) {
+    let options = {
+        url: `/post/dislike/${postId}`,
+    };
+
+    try {   
+        const res = await api.patch({}, options);
+        return res;
+    } catch (error) {
+        return error;
+    }
+}
