144450a6012051cd4009ab1f2a0fea61e6c31f3e
joaquinsalinas06 <joaquin.salinas@utec.edu.pe>
2024-06-26 11:00:05 -0500

    Añadido Funcionalidad de Añadir amigo

---
+++ src/services/friends/addFriend.js
@@ -0,0 +1,18 @@
+import Api from "../api";
+
+const api = new Api({});
+
+export async function addFriend(userId) {
+
+    let options = {
+        url: `/user/friends/add/${userId}`
+    };
+
+    try {
+        const response = await api.patch({}, options);
+        return response;
+    }
+    catch (error) {
+        throw error;
+    }
+}
---
+++ src/services/friends/isFriends.js
@@ -0,0 +1,17 @@
+import Api from "../api";
+
+const api = new Api({});    
+
+export async function isFriends(userId) {
+    let options = {
+        url: `/user/me/friends/${userId}`
+    };
+
+    try {
+        const response = await api.get(options);
+        return response;
+    }
+    catch (error) {
+        throw error;
+    }
+}
