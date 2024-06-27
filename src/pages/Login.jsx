546dcc34681696fc7f7d53b057c9a547e312d7bd
Gussephe Benjamin <gussephe.ayra@utec.edu.pe>
2024-06-27 13:35:14 -0500

    configuracion Tailwind

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -31,14 +31,25 @@ const Login = () => {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" placeholder="Email" onChange={handleChange} required />
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="password" placeholder="Password" onChange={handleChange} required />
-            <button type="submit">Logeate!</button>
-            <button onClick={() => navigate('/auth/register')}>Registrarse</button>
-        </form>
+
+        <>
+            <div className='bg-red-600'>
+
+                Inicia sesión en Mure 
+
+            
+            
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="Email" onChange={handleChange} required />
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" name="password" placeholder="Password" onChange={handleChange} required />
+                <button type="submit">Logeate!</button>
+                <button onClick={() => navigate('/auth/register')}>Registrarse</button>
+            </form>
+
+            </div>
+        </>
     )
 }
 
